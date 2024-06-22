52fe66770ad618eff466f5f1cf706fe36cbfaa2f
Abhinav Goel <abhinavgo1000@gmail.com>
2024-06-21 18:28:39 -0600

    bar chart and line chart added

--- src/app/components/pages/home-page/home-page.component.ts
+++ src/app/components/pages/home-page/home-page.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 
+import { BarChartComponent } from '../../charts/bar-chart/bar-chart.component';
+import { LineChartComponent } from '../../charts/line-chart/line-chart.component';
+
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [
+    BarChartComponent,
+    LineChartComponent
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
